35d595a3c2ccb30e94ec73db0c5aa0ebbbc147f4
Emilie Tole <eetole@gmail.com>
2021-06-09 13:13:14 -0400

    added/tested/shuffunction

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 "use strict";
+
+const shuffle = (array) => {
+  let currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+};
