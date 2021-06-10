794c2a6b35473ad140654ed9ce8fc43bb1d79c5f
Emilie Tole <eetole@gmail.com>
2021-06-10 11:34:37 -0400

    flipped cards and added setTimeout

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let listItems = document.querySelectorAll(".card");
 let newDeck = [];
 let deck = document.querySelector(".deck");
 let resetButton = document.querySelector(".reset-button");
+let deckArr = document.querySelector(".deckArr");
+let openedCards = [];
 
 listItems.forEach((item) => {
   newDeck.push(item);
@@ -33,3 +35,40 @@ const shuffleDeck = () => {
 };
 
 resetButton.addEventListener("click", shuffleDeck);
+
+const flipCard = (e) => {
+  if (e.target.classList.contains("card")) {
+    e.target.classList.toggle("flipCard");
+  }
+};
+deck.addEventListener("click", flipCard);
+
+// const unFlipCard = () => {};
+
+const cardOpen = () => {
+  openedCards.push(deckArr);
+  let length = openedCards.length;
+  if (length === 2) {
+    if (openedCards[0].type === openedCards[1].type) {
+      matched();
+    } else {
+      unmatched();
+    }
+  }
+};
+
+const matched = () => {
+  openedCards[0].classList.add("match");
+  openedCards[1].classList.add("match");
+  openedCards = [];
+};
+
+const unmatched = () => {
+  openedCards[0].classList.add("unmatched");
+  openedCards[1].classList.add("unmatched");
+  setTimeout(function () {
+    openedCards[0].classList.remove("flipCard", "unmatched");
+    openedCards[1].classList.remove("flipCard", "unmatched");
+    openedCards = [];
+  }, 1500);
+};
